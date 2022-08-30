c1b634a1f3defedcbc144911e463e83082231cab
CONFUSER <confuser@chetu.com>
2022-08-30 21:40:21 +0530

    Radiobutton implemented

--- app/src/main/java/com/chetu/demotp8/SimpleDemoActivity.java
+++ app/src/main/java/com/chetu/demotp8/SimpleDemoActivity.java
@@ -8,12 +8,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
-public class SimpleDemoActivity extends AppCompatActivity implements View.OnClickListener {
+import com.google.android.material.snackbar.Snackbar;
+
+public class SimpleDemoActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
     private Button btnClick, btnSubmit;
     private Context context;
     private EditText etInput;
+    private RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +26,10 @@ public class SimpleDemoActivity extends AppCompatActivity implements View.OnClic
         btnClick = findViewById(R.id.btn_click);
         etInput = findViewById(R.id.et_input);
         btnSubmit = findViewById(R.id.btn_submit);
+        radioGroup = findViewById(R.id.rg_gender);
         context = this;
+
+        radioGroup.setOnCheckedChangeListener(this);
 
 //        btnClick.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -45,15 +53,25 @@ public class SimpleDemoActivity extends AppCompatActivity implements View.OnClic
     @Override
     public void onClick(View view) {
         int id = view.getId();
-        if (id == R.id.btn_submit){
-            Toast.makeText(context, btnSubmit.getText().toString(), Toast.LENGTH_SHORT).show();
+        if (view.getId() == R.id.btn_submit){
+//            Toast.makeText(context, btnSubmit.getText().toString(), Toast.LENGTH_SHORT).show();
+            Snackbar.make(view, btnSubmit.getText().toString(), Snackbar.LENGTH_SHORT).show();
         }
-        if (id == R.id.btn_click){
+        if (view.getId() == R.id.btn_click){
             if (etInput.getText().toString().length() == 10) {
-                Toast.makeText(context, etInput.getText().toString(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(context, etInput.getText().toString(), Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, etInput.getText().toString(), Snackbar.LENGTH_SHORT).show();
             }else {
-                Toast.makeText(context, "input must be 10 digit", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(context, "input must be 10 digit", Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, "input must be 10 digit", Snackbar.LENGTH_SHORT).show();
             }
         }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int checkedRadioBUtton) {
+        int id = radioGroup.getCheckedRadioButtonId();
+        RadioButton radioButton = (RadioButton) findViewById(id);
+        Toast.makeText(context,  radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
